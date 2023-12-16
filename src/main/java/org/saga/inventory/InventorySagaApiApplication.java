e98c499af6871b1b31219dfb75f6c2053cdff1d3
VardanMatevosyan <vardanmt@gmail.com>
2023-12-16 18:17:19 +0200

    init changes

---
+++ src/main/java/org/saga/inventory/InventorySagaApiApplication.java
@@ -0,0 +1,13 @@
+package org.saga.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySagaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySagaApiApplication.class, args);
+	}
+
+}
